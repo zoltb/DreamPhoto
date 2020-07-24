0cdece52e89d00acf2abf9a75a25a6c39ef244c0
Zoli <zolipt@gmail.com>
2020-07-24 23:34:19 +0200

    full screen header beautify ended
    text is OK
    header responsive need to set

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,44 +1,43 @@
 import {Component, HostListener} from '@angular/core';
 
 
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
-
-
-
   cssStringVar = 'header1';
 
-
-
   public headerScrolled = false;
+  public headerShrink = false;
   @HostListener('window:scroll', ['$event'])
   onWindowScroll() {
     // const componentPosition = this.el.nativeElement.offsetTop;
     // console.log(componentPosition);
-    const num = window.pageYOffset;
-    console.log(num);
-    if (num > 199) {
+    const pageYOffset = window.pageYOffset;
+
+    if (pageYOffset > 330) {
       this.headerScrolled = true;
       this.cssStringVar = 'header2';
-    } else if (this.headerScrolled && num < 600) {
-
+    }
+    else if (this.headerScrolled && pageYOffset < 600) {
       this.headerScrolled = false;
       this.cssStringVar = 'header1';
     }
-    console.log(this.headerScrolled + 'zzzz' + num);
 
   }
-//
-//   document.getElementById("navbar").style.padding = "30px 10px";
-// //     document.getElementById("logo").style.fontSize = "25px";
-// //   } else {
-// //     document.getElementById("navbar").style.padding = "80px 10px";
-// //     document.getElementById("logo").style.fontSize = "35px";
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    const pageSize= window.innerWidth;
+    console.log(innerWidth)
+    if (pageSize < 992){
+      this.cssStringVar = 'header3';
+      this.headerShrink=true;
+
+    }
+  }
+
 
 }
